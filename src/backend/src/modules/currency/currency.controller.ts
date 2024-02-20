95a6521c78f95c3cb46b3fd4afda1c2eb493a938
ArielBritezDiaz <arielbritezdiaz@gmail.com>
2024-02-20 04:41:32 -0300

    added more filtering parameters, such as 'where' clauses for all existing fields in the table, 'order_by' for all fields, and 'order_direction' for asc or desc

--- src/backend/src/modules/currency/currency.controller.ts
+++ src/backend/src/modules/currency/currency.controller.ts
@@ -3,8 +3,7 @@ import { Request, Response, query } from 'express';
 //Service import
 import { CurrencyService } from './currency.service';
 //DTO import
-import { CurrencyValueQueryControl, CurrenciesValueQueryControl } from 'src/dto/queryControl.dto';
-
+import { CurrenciesValueControlDTO, CurrencyValueControlDTO } from './dto/currency.dto';
 
 @Controller(`api/${process.env.API_VERSION}/currency`)
 export class CurrencyController {
@@ -18,27 +17,60 @@ export class CurrencyController {
     @Req() req: Request,
     @Res() res: Response,
     @Headers('Countries_Sundries-API_Key') apiKeyHeader: string,
-    @Query() queryParams: CurrenciesValueQueryControl
+    @Query() queryParams: CurrenciesValueControlDTO
   ){
     try {
       const apiKey = parseInt(apiKeyHeader, 10);
-      // console.log(queryParams);
+      console.log(queryParams);
       const from = queryParams.from != null ? Number(queryParams.from) : 0;
       const take = queryParams.take != null ? Number(queryParams.take) : 0;
+      const id = queryParams.id != null && queryParams.id.toString() !== '0' ? Number(queryParams.id) : undefined;
+      const name = queryParams.name != null ? String(queryParams.name) : null;
+      const abbr = queryParams.abbr != null ? String(queryParams.abbr) : null;
+      const symbol = queryParams.symbol != null ? String(queryParams.symbol) : null;
+      const order_by = queryParams.order_by != null ? String(queryParams.order_by) : null;
+      const order_direction = queryParams.order_direction != null ? String(queryParams.order_direction) : null;
 
-      const query: { from: number, take: number } = {
+      const query: { from: number, take: number, id: number, name: string, abbr: string, symbol: string, order_by: string, order_direction: string } = {
         from,
-        take
+        take,
+        id,
+        name,
+        abbr,
+        symbol,
+        order_by,
+        order_direction
       };
 
-      // console.log("query:", query);
+      console.log("query:", query);
 
       //Validation
-      if (query === null || Object.keys(query).length === 0 || !query.hasOwnProperty('from') || !query.hasOwnProperty('take')) {
-        return res.status(HttpStatus.UNPROCESSABLE_ENTITY).send({ message: 'Parameters must be provided' });
-      } else if (Object.keys(query).some(key => query[key] === null || query[key] === undefined || query[key] === '' || query[key] < 0)) {
-        return res.status(HttpStatus.UNPROCESSABLE_ENTITY).send({ message: 'Invalid parameters' });
+      if (
+        query === null ||
+        Object.keys(query).length === 0 || 
+        !query.hasOwnProperty('from') || 
+        !query.hasOwnProperty('take') || 
+        !query.hasOwnProperty('name') || 
+        !query.hasOwnProperty('abbr') || 
+        !query.hasOwnProperty('symbol')
+      ) {
+          console.log(`Error: ${query} (${typeof query})`);
+          return res.status(HttpStatus.UNPROCESSABLE_ENTITY).send({ message: 'Parameters must be provided' });
+      } else if (
+          Object.keys(query).some(key => query[key] === '' || query[key] < 0) || 
+          query.from < 0 || 
+          query.take < 0 ||
+          (query.id !== undefined && isNaN(query.id)) || 
+          (query.id !== undefined && (query.id < 0 || query.id > 220)) || 
+          (query.name !== null && (query.name && (query.name.length < 3 || query.name.length > 40))) ||
+          (query.abbr !== null && (query.abbr !== '' && query.abbr.length < 2 || query.abbr.length > 3 )) ||
+          (query.symbol !== null && (query.symbol && (query.symbol.length < 1 || query.symbol.length > 30 ))) ||
+          (query.order_by !== null && (query.order_by !== '' && query.order_by !== 'id_currency' && query.order_by !== 'name' && query.order_by !== 'abbr' && query.order_by !== 'symbol')) ||
+          (query.order_direction !== null && (query.order_direction !== '' && query.order_direction !== 'asc' && query.order_direction !== 'desc'))
+      ) {
+          return res.status(HttpStatus.UNPROCESSABLE_ENTITY).send({ message: 'Invalid parameters' });
       }
+      
       if (apiKey !== 123) {
         console.log(`Error: ${apiKey} (${typeof apiKey})`);
         return res.status(HttpStatus.UNAUTHORIZED).send({ message: 'Api-Key is incorrect' });
@@ -60,7 +92,7 @@ export class CurrencyController {
     @Res() res: Response,
     @Headers('Countries_Sundries-API_Key') apiKeyHeader: string,
     @Param() params: any,
-    @Query() queryParams: CurrencyValueQueryControl
+    @Query() queryParams: CurrencyValueControlDTO
   ){
     try {
       const apiKey = parseInt(apiKeyHeader, 10)
--- src/backend/src/modules/currency/currency.service.ts
+++ src/backend/src/modules/currency/currency.service.ts
@@ -4,6 +4,9 @@ import { Currency } from '@prisma/client';
 import { DatabaseConfig } from '../../interfaces/database.config.interface';
 import { PrismaService } from '../prisma/prisma.service';
 
+//DTO import
+import { CurrenciesValueControlDTO } from './dto/currency.dto';
+
 @Injectable()
 export class CurrencyService {
   constructor(
@@ -11,29 +14,54 @@ export class CurrencyService {
     private configService: ConfigService<{ database: DatabaseConfig }, true>
   ) {}
 
-  async getAllCurrencies(preferencefrom: any): Promise<Currency[]> {
+  async getAllCurrencies(preferencesParams: CurrenciesValueControlDTO): Promise<Currency[]> {
     
-    console.log("preferencefrom", preferencefrom)
+    console.log("preferencesParams", preferencesParams)
 
     let skipFrom = {}
     let takeTake = {}
-    if(preferencefrom.from !== 0) {
-      skipFrom = { skip: preferencefrom.from }
-    }
-    if(preferencefrom.to !== 0) {
-      takeTake = { take: preferencefrom.take }
-    }
+    let where = {
+      id_currency: { contains: 0},
+      name: { contains: '' },
+      abbr: { contains: '' },
+      symbol: { contains: '' }
+    };
+    let order_by = {}
+    let order_direction = {}
+    
+    if (preferencesParams.from !== 0) { skipFrom = { skip: preferencesParams.from } }
+    if (preferencesParams.take !== 0) { takeTake = { take: preferencesParams.take } }
+    if (preferencesParams.id !== 0) { where.id_currency = { contains: preferencesParams.id } }
+    if (preferencesParams.name !== null) { where.name = { contains: preferencesParams.name.toUpperCase() } }
+    if (preferencesParams.abbr !== null) { where.abbr = { contains: preferencesParams.abbr.toUpperCase() } }
+    if (preferencesParams.symbol !== null) { where.symbol = { contains: preferencesParams.symbol } }
+    if (preferencesParams.order_by !== null) { order_by = { orderBy: preferencesParams.order_by } }
+    if (preferencesParams.order_direction !== null) { order_direction = preferencesParams.order_direction }
+    
+    // console.log("where", where);
+    console.log("order_by", order_by);
+    console.log("order_direction", order_direction);
 
     const response = await this.prisma.currency.findMany({
       ...skipFrom,
       ...takeTake,
+      where: {
+        ...(preferencesParams.id !== 0 && { id_currency: preferencesParams.id }),
+        ...(preferencesParams.name !== null && { name: { contains: preferencesParams.name.toUpperCase() } }),
+        ...(preferencesParams.abbr !== null && { abbr: { contains: preferencesParams.abbr.toUpperCase() } }),
+        ...(preferencesParams.symbol !== null && { symbol: { contains: preferencesParams.symbol } }),
+      },
       select: {
         id_currency: true,
         name: true,
         abbr: true,
         symbol: true
+      },
+      orderBy: {
+        ...(preferencesParams.order_by !== null && { [preferencesParams.order_by]: preferencesParams.order_direction })
       }
     });
+    // console.log("response", response)
 
     return response;
   }
