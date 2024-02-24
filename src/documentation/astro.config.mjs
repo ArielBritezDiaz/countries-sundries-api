23b60a7251f2d193edd7a267af44f1827446d6ab
ArielBritezDiaz <arielbritezdiaz@gmail.com>
2024-02-24 17:07:42 -0300

    get data from db to show in docs

--- src/documentation/astro.config.mjs
+++ src/documentation/astro.config.mjs
@@ -1,46 +1,58 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: '',
-			customCss: [
-				'./src/styles/global.css',
-			],
-			social: {
-				linkedin: 'https://www.linkedin.com/in/ariel-britez-diaz-technical/',
-				github: 'https://github.com/ArielBritezDiaz'
-			},
-			logo: {
-				src: './src/assets/logo.svg'
-			},
-			sidebar: [
-				{
-					label: 'Introduction',
-					items: [
-						{ label: 'Overview', link: '/docs/introduction/overview/' },
-						{ label: 'Get API-Key access', link: '/docs/introduction/get-api-key/' },
-						{ label: '¿Why Countries Sundries?', link: '/docs/introduction/why-countries-sundries/' },
-						{ label: 'Concepts and diagrams', link: '/docs/introduction/concept/' },
-					],
-				},
-				{
-					label: 'Getting Started',
-					items: [
-						{ label: 'Getting Started', link: '/docs/getting-started/getting-started' },
-						{ label: 'Basic Usage', link: '/docs/getting-started/basic-usage/' },
-						{ label: 'Supported Data', link: '/docs/getting-started/supported-data' }
-					]
-				},
-				{
-					label: 'API Docs',
-					items: [
-						{ label: 'Country', link: '/docs/api-docs/country/' },
-					]
-				}
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: '',
+    customCss: [
+			'./src/styles/global.css',
+			'./src/styles/country-api-docs.css',
+		],
+    social: {
+      linkedin: 'https://www.linkedin.com/in/ariel-britez-diaz-technical/',
+      github: 'https://github.com/ArielBritezDiaz'
+    },
+    logo: {
+      src: './src/assets/logo.svg'
+    },
+    sidebar: [{
+      label: 'Introduction',
+      items: [{
+        label: 'Overview',
+        link: '/docs/introduction/overview/'
+      }, {
+        label: 'Get API-Key access',
+        link: '/docs/introduction/get-api-key/'
+      }, {
+        label: '¿Why Countries Sundries?',
+        link: '/docs/introduction/why-countries-sundries/'
+      }, {
+        label: 'Concepts and diagrams',
+        link: '/docs/introduction/concept/'
+      }]
+    }, {
+      label: 'Getting Started',
+      items: [{
+        label: 'Getting Started',
+        link: '/docs/getting-started/getting-started'
+      }, {
+        label: 'Basic Usage',
+        link: '/docs/getting-started/basic-usage/'
+      }, {
+        label: 'Supported Data',
+        link: '/docs/getting-started/supported-data'
+      }]
+    }, {
+      label: 'API Docs',
+      items: [{
+        label: 'Country',
+        link: '/docs/api-docs/country/'
+      }]
+    }]
+  }), tailwind({
+		applyBaseStyles: false,
+	})]
 });
--- src/documentation/src/content/docs/docs/getting-started/basic-usage.md
+++ src/documentation/src/content/docs/docs/getting-started/basic-usage.md
@@ -1,3 +1,17 @@
 ---
 title: 'Getting Started'
 ---
+
+<!-- const allCountries = async () => {
+  const response = await fetch('http://localhost:3000/api/v1/country/all', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-countries_sundries_api-key': '007'
+    }
+  })
+  const data = await response.json()
+  console.log(data)
+}
+
+allCountries() -->
