32dcb09fed14fd3d191a4277d8e091a264efa0a7
ArielBritezDiaz <arielbritezdiaz@gmail.com>
2024-03-01 02:42:02 -0300

    guard for the api-version correct in headers, added

--- src/backend/src/guard/api-version.guard.ts
+++ src/backend/src/guard/api-version.guard.ts
@@ -1,17 +1,22 @@
-// import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
-// import { FastifyRequest } from "fastify";
-// import { Observable } from "rxjs";
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { FastifyRequest } from "fastify";
+import { Observable } from "rxjs";
 
-// @Injectable()
-// export class ApiVersionGuard implements CanActivate {
-//   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-//     const req = context.switchToHttp().getRequest<FastifyRequest>()
-//     const requestedVersion = req.headers['x-api-version']
+@Injectable()
+export class ApiVersionGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const req = context.switchToHttp().getRequest<FastifyRequest>()
+    const requestedVersion = req.headers['x-api-version']
 
-//     if(!requestedVersion || !SUPPORTED_VERSIONS.includes(requestedVersion)) {
-//       return false
-//     }
+    const SUPPORTED_VERSIONS = ['1'] // This is the supported versions of the API, same as the defaultVersion in main.ts
 
-//     return true
-//   }
-// }
+    if(!requestedVersion || !SUPPORTED_VERSIONS.includes(requestedVersion.toString())) {
+      console.log('Unsupported version')
+      console.log('requestedVersion:', requestedVersion)
+      throw new UnauthorizedException('API version incorrect')
+    }
+    // console.log('Supported version')
+    // console.log('requestedVersion:', requestedVersion)
+    return true
+  }
+}
--- src/backend/src/main.ts
+++ src/backend/src/main.ts
@@ -9,15 +9,17 @@ import { HttpException, HttpStatus, ValidationPipe, VersioningType } from '@nest
 //Fastify platfom for versioning API
 import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { FastifyRequest } from 'fastify';
+import { ApiVersionGuard } from './guard/api-version.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter());
   app.setGlobalPrefix('api') // global prefix for 'countriessundriesapi.com/api'
   app.enableVersioning({
     type: VersioningType.URI, // default prefix is 'v', and this complements with the @Version decorator in the controllers methods, for now is set to version '1'.
-    defaultVersion: '1'
+    defaultVersion: '1' // default version of the API
   })
   //Guard use
+  app.useGlobalGuards(new ApiVersionGuard()); // after of the apiKeyGuard, validate the version of the API, if the version is not supported, the guard throw an UnauthorizedException, and if the version is supported, the guard return true and the request continue to the next middleware
   app.useGlobalGuards(new ApiKeyGuard());
   //Pipe use
   app.useGlobalPipes(new ValidationPipe({
