e810be3d885957060c3c3733ce4717efa945f629
ArielBritezDiaz <arielbritezdiaz@gmail.com>
2024-03-01 03:34:38 -0300

    first successful cache implementation with cacheManager, Redis coming soon...

--- src/backend/src/module/country/country.module.ts
+++ src/backend/src/module/country/country.module.ts
@@ -2,7 +2,11 @@ import { Module } from "@nestjs/common";
 import { CountryController } from "./country.controller";
 import { CountryService } from "./country.service";
 
+//Caching import
+import { CacheModule } from '@nestjs/cache-manager';
+
 @Module({
+  imports: [CacheModule.register()], // cache-manager import for caching
   controllers: [CountryController],
   providers: [CountryService]
 })
