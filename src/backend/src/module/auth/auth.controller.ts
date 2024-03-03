03150c0b4de854594bd11d1de940d70b1d4f3b0b
ArielBritezDiaz <arielbritezdiaz@gmail.com>
2024-03-03 20:34:31 -0300

    endpoint 'test' for token testing in auth.controller removed

--- src/backend/src/module/auth/auth.controller.ts
+++ src/backend/src/module/auth/auth.controller.ts
@@ -27,10 +27,4 @@ export class AuthController {
       throw new UnauthorizedException('Invalid credentials');
     }
   }
-
-  @UseGuards(AuthGuard)
-  @Get('test')
-  test() {
-    return 'Hello World'
-  }
 }
