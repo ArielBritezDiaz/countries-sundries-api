ca4e11b790b1448ead03767850a86c30865788c0
ArielBritezDiaz <arielbritezdiaz@gmail.com>
2024-03-13 17:48:42 -0300

    docs: general organization and optimization for light and dark mode

---
+++ src/documentation/.vercel/output/functions/_render.func/home/arieldiaz/Desktop/dev/code/personal/countriesSundriesApi/src/documentation/.vercel/output/_functions/chunks/generic_DWE4J59e.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/generic_CB3G66m3.mjs').then(n => n.g);
+
+export { page };
---
+++ src/documentation/.vercel/output/functions/_render.func/home/arieldiaz/Desktop/dev/code/personal/countriesSundriesApi/src/documentation/.vercel/output/_functions/chunks/profile_DWEBdjbD.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/profile_CvGKIgNp.mjs').then(n => n.p);
+
+export { page };
