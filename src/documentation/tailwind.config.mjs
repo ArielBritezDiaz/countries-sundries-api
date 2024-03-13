d355f4130aefde7b67bc661482c0f2972f1a8f3a
ArielBritezDiaz <arielbritezdiaz@gmail.com>
2024-03-12 23:41:46 -0300

    docs: style general changed | 'profile' section almost completed

--- src/documentation/tailwind.config.mjs
+++ src/documentation/tailwind.config.mjs
@@ -1,24 +1,11 @@
 import starlightPlugin from '@astrojs/starlight-tailwind';
-import colors from 'tailwindcss/colors';
 const { addDynamicIconSelectors } = require('@iconify/tailwind');
-
-const accent = { 200: '#375e8d', 950: '#fff' };
-const gray = { 100: '#f3f7f9', 200: '#e7eff2', 300: '#bac4c8', 400: '#7b8f96', 700: '#1a1b26', 800: '#2d4056', 900: '#162332' };
-
-//002d35
-//143f47
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-      // colors: {
-      //   // Tu color de acento preferido. Indigo es el más cercano a los valores predeterminados de Starlight.
-      //   accent,
-      //   // Tu escala de grises preferida. Zinc es el más cercano a los valores predeterminados de Starlight.
-      //   gray,
-      // },
       fontFamily: {
         lexend_500: ['lexend-500'],
         lexend_400: ['lexend-400'],
