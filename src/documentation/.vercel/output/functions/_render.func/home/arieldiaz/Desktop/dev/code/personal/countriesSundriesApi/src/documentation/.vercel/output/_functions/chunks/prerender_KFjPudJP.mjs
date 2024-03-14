fc7bbf934a9a85659143268ed9a9956a85dabd12
ArielBritezDiaz <arielbritezdiaz@gmail.com>
2024-03-14 20:10:51 -0300

    restart

--- src/documentation/.vercel/output/functions/_render.func/home/arieldiaz/Desktop/dev/code/personal/countriesSundriesApi/src/documentation/.vercel/output/_functions/chunks/prerender_KFjPudJP.mjs
+++
@@ -1,7 +0,0 @@
-const noop = () => {};
-export const $ = noop;
-export const _ = noop;
-export const a = noop;
-export const i = noop;
-export const s = noop;
-export const u = noop;
--- src/documentation/.vercel/output/static/_astro/google.astro_astro_type_script_index_0_lang.oQCVZvzI.js
+++
@@ -1,1 +0,0 @@
-const o=await fetch("http://localhost:3000/v1/auth/google/login",{method:"GET",headers:{"x-api-version":"1","Content-Type":"application/json"}});console.log("response",o);
