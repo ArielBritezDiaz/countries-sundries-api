d5efbf04a19f6c6feaa15a9b8755429c5c59759d
ArielBritezDiaz <arielbritezdiaz@gmail.com>
2024-03-15 10:41:53 -0300

    api: prisma generate

--- src/api/src/generated/client/index.d.ts
+++ src/api/src/generated/client/index.d.ts
@@ -312,8 +312,8 @@ export namespace Prisma {
   export import Exact = $Public.Exact
 
   /**
-   * Prisma Client JS version: 5.11.0
-   * Query Engine version: efd2449663b3d73d637ea1fd226bafbcf45b3102
+   * Prisma Client JS version: 5.10.2
+   * Query Engine version: 5a9203d0590c951969e85a7d07215503f4672eb9
    */
   export type PrismaVersion = {
     client: string
